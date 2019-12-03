a86e71a7586a94cd2a9a6a5bd97890fb48abd3ab
Quentin Castel <quentin.castel@forgerock.com>
2019-12-03 16:27:41 +0000

    Fixing the conditions for the reviewers

--- backport.sh
+++ backport.sh
@@ -95,7 +95,7 @@ for branch in $(git branch -r | grep ${BACKPORT_BRANCHES_REGEX} | sed 's/origin\
             \"base\": \"${branch}\"
           }")
 
-    if [[ -z $reviewers ]]; then
+    if [[ -n $reviewers ]]; then
       echo "Add reviewers"
       pull_request_id=$(echo response | jq .number)
       curl -X POST \
